17f7433a6e150352c5ad68d1ae3b2807e65c375f
JaromirKapitan <jaromir.kapitan1@gmail.com>
2025-08-28 21:40:44 +0200

    images - pridavanie a odoberanie (podobne potom subory)

--- app/Livewire/Admin/MediaImages.php
+++ app/Livewire/Admin/MediaImages.php
@@ -2,14 +2,42 @@
 
 namespace App\Livewire\Admin;
 
+use Livewire\Attributes\Validate;
 use Livewire\Component;
+use Livewire\WithFileUploads;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class MediaImages extends Component
 {
+    use WithFileUploads;
+
     public $model;
+
+    #[Validate(['images.*' => 'image|max:10240'])]
+    public $images = [];
 
     public function render()
     {
         return view('livewire.admin.media-images');
+    }
+
+    public function updatedImages(){
+        $this->validate();
+
+        if(empty($this->images)) return false;
+
+        /** @var \Livewire\Features\SupportFileUploads\TemporaryUploadedFile $image */
+        foreach($this->images as $image){
+            $stored = $image->store('images');
+            $path = storage_path('app/private/' . $stored);
+            $this->model->addMedia($path)->toMediaCollection('images');
+        }
+
+        $this->reset('images');
+    }
+
+    public function delete(Media $image)
+    {
+        $image->delete();
     }
 }
--- resources/views/livewire/admin/media-images.blade.php
+++ resources/views/livewire/admin/media-images.blade.php
@@ -1,13 +1,48 @@
 <div class="container-fluid">
 
-    <div class="row">
-        <div class="col-md-9">
-            Images list
+    <form wire:submit="store" class="mb-3">
+        <div
+            x-data="{ uploading: false, progress: 0 }"
+            x-on:livewire-upload-start="uploading = true"
+            x-on:livewire-upload-finish="uploading = false"
+            x-on:livewire-upload-cancel="uploading = false"
+            x-on:livewire-upload-error="uploading = false"
+            x-on:livewire-upload-progress="progress = $event.detail.progress"
+        >
+
+            <input class="form-control" type="file" multiple wire:model="images"/>
+            @error('images')
+            <span class="alert alert-error">{{ $message }}</span>
+            @enderror
+
+            <!-- Progress Bar -->
+            <div x-show="uploading">
+                <progress max="100" x-bind:value="progress"></progress>
+            </div>
         </div>
-        <div class="col-md-3">
-            DROPZONE
-        </div>
+    </form>
+
+    <div class="row g-3">
+        @foreach($model->getMedia('images') as $image)
+            <div class="col-sm-6 col-md-3 col-lg-2">
+                <div class="card h-100">
+                    <img src="{{ $image->getUrl() }}" class="img-thumbnail" alt="..."/>
+                    <div class="card-body d-flex align-items-end">
+                        <div class="btn-group btn-group-sm w-100" role="group" aria-label="Small button group">
+                            <button type="button" class="btn btn-outline-decondary disabled">
+                                <i class="fa fa-arrows-up-down-left-right"></i>
+                            </button>
+                            <button type="button" class="btn btn-outline-decondary disabled">
+                                <i class="fa fa-pencil"></i>
+                            </button>
+                            <button type="button" class="btn btn-outline-danger" wire:click="delete({{$image->id}})">
+                                <i class="fa fa-trash-can"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
     </div>
 
-{{--    {{ dd($model) }}--}}
 </div>
