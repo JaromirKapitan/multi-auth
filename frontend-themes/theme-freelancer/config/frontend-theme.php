dd7c8cd6800fdfcf7d463e842e16160d1bbd5ba0
JaromirKapitan <jaromir.kapitan1@gmail.com>
2025-09-09 22:25:14 +0200

    frontend-themes/theme-freelancer - init

--- frontend-themes/theme-freelancer/config/frontend-theme.php
+++ frontend-themes/theme-freelancer/config/frontend-theme.php
@@ -7,4 +7,8 @@ return [
     'js' => [
         'frontend-themes/theme-freelancer/resources/js/app.js',
     ],
+    'assets' => [
+        'source' => 'frontend-themes/theme-freelancer/resources/assets',
+        'target' => 'public/assets',
+    ],
 ];
